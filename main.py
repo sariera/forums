3042e46b3be43f751cfd6f1ada62e23c913dfeac
Ali Atallah Faris Al Sariera <sariera@live.com>
2018-02-22 13:15:36 +0200

    Topic 13 Solution

---
+++ main.py
@@ -0,0 +1,8 @@
+from models import Member, Post
+
+member1 = Member("Ali", 30)
+member2 = Member("Rawan", 27)
+
+post1 = Post("Topic 1", "This is the First Topic")
+post2 = Post("Topic 2", "This is the Second Topic")
+post3 = Post("Topic 3", "This is the Third Topic")
---
+++ models.py
@@ -0,0 +1,14 @@
+class Member:
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        print "Member Class Initialized"
+
+class Post:
+
+    def __init__(self, title, content):
+        self.title = title
+        self.content = content
+        print "Post Class Initialized"
+
